6324014efd4858310de21a5ff57986325b959439
Loïc Faugeron <faugeron.loic+git@gmail.com>
2015-09-19 13:10:58 +0100

    Made importer optional

--- app/config/importer.php
+++ app/config/importer.php
@@ -1,9 +1,11 @@
 <?php
 
-use Symfony\Component\Finder\Finder;
+// Import custom configuration files: service definitions, vendor configurations, etc.
 
-$finder = new Finder();
-$files = $finder->files()->name('*.yml')->in(__DIR__.'/services')->in(__DIR__.'/vendors');
-foreach ($files as $file) {
-    $loader->import($file->getRealpath());
-}
+// use Symfony\Component\Finder\Finder;
+// 
+// $finder = new Finder();
+// $files = $finder->files()->name('*.yml')->in(__DIR__.'/services')->in(__DIR__.'/vendors');
+// foreach ($files as $file) {
+//     $loader->import($file->getRealpath());
+// }
