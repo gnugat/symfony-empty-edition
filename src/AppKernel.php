33424b83f4a10576c5a4fb2896e51389e1df835e
Loïc Faugeron <faugeron.loic@gmail.com>
2024-11-18 19:58:30 +0000

    Added Route Attributes, and Autowiring

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -7,37 +7,35 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
-    public function registerBundles(): \Generator
+    #[\Override]
+    public function registerBundles(): iterable
     {
+        // Taken from symfony/framework-bundle's MicroKernelTrait
         $contents = require __DIR__.'/../config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
         }
     }
 
-    public function getProjectDir(): string
-    {
-        return __DIR__;
-    }
-
-    public function getCacheDir(): string
-    {
-        return __DIR__.'/../var/cache/'.$this->environment;
-    }
-
-    public function getLogDir(): string
-    {
-        return __DIR__.'/../var/logs';
-    }
-
+    #[\Override]
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $configFilename = __DIR__.'/../config/config.yaml';
-        if ('test' === $this->environment) {
-            $configFilename = __DIR__.'/../config/config_test.yaml';
+        // Configure container
+        $configFilename = __DIR__.'/../config/services.yaml';
+        $configEnvFilename = __DIR__."/../config/services_{$this->environment}.yaml";
+        if (is_file($configEnvFilename)) {
+            $configFilename = $configEnvFilename;
         }
         $loader->load($configFilename);
+    }
+
+    #[\Override]
+    public function getProjectDir(): string
+    {
+        // Points to the app's path where the `composer.json` is.
+        // An implemenation for this is already provided in `Kernel`, but here we provide a solution that's more simple.
+        return __DIR__.'/..';
     }
 }
